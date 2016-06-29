4fc7459302c25a103f5096698202577a3d8d502f
oakes <zsoakes@gmail.com>
2016-06-29 10:06:18 -0400

    Add logout

--- src/main/java/com/theironyard/controllers/IronGramRestController.java
+++ src/main/java/com/theironyard/controllers/IronGramRestController.java
@@ -50,4 +50,15 @@ public class IronGramRestController {
         User user = users.findFirstByName(username);
         return photos.findByRecipient(user);
     }
+
+    @RequestMapping(path = "/user", method = RequestMethod.GET)
+    public User getUser(HttpSession session) {
+        String username = (String) session.getAttribute("username");
+        if (username == null) {
+            return null;
+        }
+        else {
+            return users.findFirstByName(username);
+        }
+    }
 }
